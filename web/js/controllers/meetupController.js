2d7c717e45b1dacd5d2fdc0ff4985fda9e458326
btz1211 <baote.zheng@gmail.com>
2016-04-05 22:23:51 -0400

    moved add friend to meetup logic to view meetup page
    moved add friend to meetup logic to view meetup page

--- web/js/controllers/meetupController.js
+++ web/js/controllers/meetupController.js
@@ -1,12 +1,18 @@
 'use strict';
 
-myApp.controller('meetupCtrl', function($scope, $routeParams, $log, mapService, meetupApiService){
-  console.log('route param:' + $routeParams.meetupId);
-  $scope.meetup = {};
-  $scope.mapElement = document.getElementById('map');
+myApp.controller('meetupCtrl', function($scope, $cookies, $routeParams, $log, mapService, meetupApiService){
+  $scope.loggedInUser = $cookies.getObject('loggedInUser');
+
+  //map
   $scope.map = {};
+  $scope.mapElement = document.getElementById('map');
   $scope.meetupMarker = {};
   $scope.meetperMarkers = [];
+
+  //meetup
+  $scope.meetup = {};
+  $scope.friends = [];
+  $scope.selectedFriends = [];
 
   meetupApiService.getMeetup($routeParams.meetupId)
   .$promise.then(function(response){
@@ -21,15 +27,47 @@ myApp.controller('meetupCtrl', function($scope, $routeParams, $log, mapService, 
     var meetuper;
     for(var i = 0; i < $scope.meetup.meetupers.length; ++i){
       meetuper = $scope.meetup.meetupers[i];
-
       if(meetuper.latitude && meetuper.longitude){
         $scope.meetuperMarkers.push(mapService.addMarker($scope.map, meetuper.lastKnownLatitude, meetuper.lastKnownLongitude, meetuper.lastName + "," +meetuper.firstName));
       }
     }
-
   }).catch(
     function(error){
       $log.warn(error);
   });
 
+  $scope.getFriends = function(){
+    //getting the last user in the friends array
+    //necessary for fast paging in mongodb
+    var friendCount = $scope.friends.length;
+    var lastUserId = friendCount > 0 ? $scope.friends[friendCount - 1]._id : null;
+
+    meetupApiService.getFriends($scope.loggedInUser._id, lastUserId, 1)
+    .$promise.then(function(response){
+      console.log('next batch of friends:' + JSON.stringify(response.data));
+      $scope.friends = response.data;
+    }).catch(function(error){
+      $log.warn(error);
+    });
+  }
+
+  $scope.searchFriends = function(searchString){
+    return meetupApiService.searchFriends($scope.loggedInUser._id, searchString)
+    .$promise.then(function(response){
+      return response.data.map(function(user){
+        user.fullName = user.firstName + ' ' + user.lastName;
+        return user;
+      })
+    }).catch(function(error){
+      $log.warn(error);
+    });
+  }
+
+  $scope.onFriendSelected = function(item, model, label){
+    $log.info("item:"+JSON.stringify(item) + ":"+JSON.stringify(model) + ":"+JSON.stringify(label));
+    $scope.searchString = "";
+    $scope.selectedFriends.push(item);
+  }
+
+  $scope.getFriends();
 });
